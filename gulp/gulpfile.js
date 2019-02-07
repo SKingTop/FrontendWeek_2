e603077f0d037e7fbab1be0021401acd4382182c
SKing <secret.king.1996@gmail.com>
2019-02-07 18:07:15 +0600

    gulp settings 07.02.2019 18:07

---
+++ gulp/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		browserSync = require('browser-sync');;
+
+gulp.task('sass', function(){ 
+    return gulp.src('../src/sass/**/*.sass') 
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('../src/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('../src/sass/**/*.sass', gulp.parallel('sass'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: '../src'
+        },
+        notify: false
+    });
+});
+
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
