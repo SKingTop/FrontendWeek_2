31270f82ea5a1939934a290998d567396bd85049
SKing <secret.king.1996@gmail.com>
2019-02-08 21:20:22 +0600

    little changes 21:20 08.02.2019

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
       var currentSlideIndex = $('.slider-item.current').index();
       var nextSlideIndex = currentSlideIndex + 1;
       var nextSlide = $('.slider-item').eq(nextSlideIndex);
-      currentSlide.hide(500);
+      currentSlide.hide(500); //hide - fadeOut show-fadeIn
       currentSlide.removeClass('current');
 
       if(nextSlideIndex == ($('.slider-item:last').index() + 1 )) {
-        $('.slider-item').eq(0).show(500);
+        $('.slider-item').eq(0).show(500);//hide - fadeOut show-fadeIn
         $('.slider-item').eq(0).addClass('current');
       } else {
-        nextSlide.show(500);
+        nextSlide.show(500);//hide - fadeOut show-fadeIn
         nextSlide.addClass('current');
       }
     });
@@ -22,9 +22,9 @@ $(document).ready(function(){
       var prevSlideIndex = currentSlideIndex - 1;
       var prevSlide = $('.slider-item').eq(prevSlideIndex);
 
-      currentSlide.hide(500);
+      currentSlide.hide(500);//hide - fadeOut show-fadeIn
       currentSlide.removeClass('current');
-      prevSlide.show(500);
+      prevSlide.show(500);//hide - fadeOut show-fadeIn
       prevSlide.addClass('current');
     });
 });
